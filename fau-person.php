51698057736eaadc9cd6047b65ba47d4e6504f22
endv02 <karin.kimpan@fau.de>
2016-04-18 15:31:08 +0200

    Automatische Korrektur der Faxnummer

--- fau-person.php
+++ fau-person.php
@@ -4,7 +4,7 @@
  Plugin Name: FAU Person
  Plugin URI: https://github.com/RRZE-Webteam/fau-person
  * Description: Visitenkarten-Plugin für FAU Webauftritte
- * Version: 2.1.1
+ * Version: 2.1.2
  * Author: RRZE-Webteam
  * Author URI: http://blogs.fau.de/webworking/
  * License: GPLv2 or later
