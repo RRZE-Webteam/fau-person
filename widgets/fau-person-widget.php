e097d8214276bda297420d3728c87391ea88e8e1
Wolfgang Wiese <xwolf@xwolf.de>
2016-08-25 10:24:42 +0200

    Bugfix #159

--- widgets/fau-person-widget.php
+++ widgets/fau-person-widget.php
@@ -58,7 +58,7 @@ class FAUPersonWidget extends WP_Widget {
 
         echo $before_widget;
         $id = empty($instance['id']) ? ' ' : $instance['id'];
-        $title = empty($instance['title']) ? ' ' : $instance['title'];
+        $title = empty($instance['title']) ? '' : $instance['title'];
         // Welche Felder sollen in der Sidebar angezeigt werden?
         $options = FAU_Person::$options['sidebar'];
         // fau_person_sidebar($id, $title, list 0, inst 1, abtielung 1, posi 1, titel 1, suffix 1, addresse 1, raum 1, tele 1, fax 1, handy 0, mail 1, url 1, mehrlink 0, kurzauszug 1, office 1, pubs 0, bild 1, via 0)
