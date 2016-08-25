d6c86cb4228de4a814d3eb56ff405bc2bf68adeb
Wolfgang Wiese <xwolf@xwolf.de>
2016-08-25 11:55:35 +0200

    Bild in widget nun parametrisierbar über parameter bild

--- widgets/fau-person-widget.php
+++ widgets/fau-person-widget.php
@@ -54,15 +54,17 @@ class FAUPersonWidget extends WP_Widget {
     }
 
     public function widget($args, $instance) {
+	$options = FAU_Person::$options['sidebar'];
         extract($args, EXTR_SKIP);
 
         echo $before_widget;
         $id = empty($instance['id']) ? ' ' : $instance['id'];
         $title = empty($instance['title']) ? '' : $instance['title'];
+	$bild = empty($instance['bild']) ? $options['bild'] : $instance['bild'];
         // Welche Felder sollen in der Sidebar angezeigt werden?
-        $options = FAU_Person::$options['sidebar'];
+       
         // fau_person_sidebar($id, $title, list 0, inst 1, abtielung 1, posi 1, titel 1, suffix 1, addresse 1, raum 1, tele 1, fax 1, handy 0, mail 1, url 1, mehrlink 0, kurzauszug 1, office 1, pubs 0, bild 1, via 0)
-        echo FAU_Person_Shortcodes::fau_person_sidebar($id, $title, 0, $options['organisation'], $options['abteilung'], $options['position'], 1, 1, $options['adresse'], 1, $options['telefon'], $options['fax'], 0, $options['mail'], $options['webseite'], 0, $options['kurzauszug'], $options['sprechzeiten'], 0, $options['bild'], 0);
+        echo FAU_Person_Shortcodes::fau_person_sidebar($id, $title, 0, $options['organisation'], $options['abteilung'], $options['position'], 1, 1, $options['adresse'], 1, $options['telefon'], $options['fax'], 0, $options['mail'], $options['webseite'], 0, $options['kurzauszug'], $options['sprechzeiten'], 0, $bild, 0);
         echo $after_widget;
     }
 
