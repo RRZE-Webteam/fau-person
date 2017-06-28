c0093476fbfa0042a896a977429df95323cc5ea4
endv02 <karin.kimpan@fau.de>
2017-06-28 17:08:41 +0200

    gmail-schnittstelle rausgenommen

--- fau-person.php
+++ fau-person.php
@@ -597,7 +597,6 @@ class FAU_Person {
                         //$output = __('UnivIS-ID', FAU_PERSON_TEXTDOMAIN) . ' '. $id . ': '. $firstname . ' ' . $lastname . ', ' . __('E-Mail', FAU_PERSON_TEXTDOMAIN) . ': ' . $email. ', ' . __('Organisation', FAU_PERSON_TEXTDOMAIN) . ': ' . $orgname;
                         echo 'UnivIS-ID '. $id . ': '. $firstname . ' ' . $lastname . ', E-Mail: ' . $email. ', Organisation: ' . $orgname;
                         echo "<br>";
-                        do_action('rrze_gmail_output');
                     }
                 }
             ?>
