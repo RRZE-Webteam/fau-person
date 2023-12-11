6fd160002010bf02053dad0e0aaef2572a2966c8
rvdforst <rolf.v.d.forst@fau.de>
2023-12-11 17:34:32 +0100

    Update Kontakt.php

--- includes/Widgets/Kontakt.php
+++ includes/Widgets/Kontakt.php
@@ -1,34 +1,34 @@
 <?php
 
 namespace FAU_Person\Widgets;
+
 defined('ABSPATH') || exit;
 
-use WP_Widget;
 /**
  * Define Image Sizes
  */
-class Kontakt extends \WP_Widget {
+class Kontakt extends \WP_Widget
+{
     protected $pluginFile;
-    private $settings;
+    protected $settings;
 
-    public function __construct($pluginFile, $settings) {
-	$this->pluginFile = $pluginFile;
+    public function __construct($pluginFile, $settings)
+    {
+        $this->pluginFile = $pluginFile;
         $this->settings = $settings;
-	
-
     }
 
 
-    public function onLoaded()    {
-	add_action( 'widgets_init', array( $this, 'register_widgets' ) );
-	       
-	  // Unterstützung vom Shortcode im Widget   
-	add_filter('widget_text','do_shortcode');
-    }
-    
-    public static function register_widgets() {
-	require_once('kontakt-widget.php');
-	register_widget( 'FAUPersonWidget' );
+    public function onLoaded()
+    {
+        add_action('widgets_init', array($this, 'register_widgets'));
+
+        // Unterstützung vom Shortcode im Widget   
+        add_filter('widget_text', 'do_shortcode');
     }
 
+    public static function register_widgets()
+    {
+        register_widget('\FAU_Person\Widgets\KontaktWidget');
+    }
 }
